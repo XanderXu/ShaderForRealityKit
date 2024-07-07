2504310876837057049da5748c74e37cdf313726
xander <xander-xu@foxmail.com>
2024-07-07 14:47:35 +0800

    noise

---
+++ ShaderForRealityKit/S02_Noise/S02_NoiseApp.swift
@@ -0,0 +1,33 @@
+//
+//  S02_NoiseApp.swift
+//  S02-Noise
+//
+//  Created by 许同学 on 2024/7/7.
+//
+
+import SwiftUI
+
+@main
+struct S02_NoiseApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
